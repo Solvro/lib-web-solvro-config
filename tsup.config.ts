e8ebae708a4a403def48f0f733ca46e672b6aced
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2024-12-03 12:20:06 +0100

    fix: move things around

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["eslint-config/index.ts", "prettier-config/index.ts"],
-  shims: true,
-  clean: true,
-  dts: true,
-  format: ["cjs", "esm"],
-  sourcemap: true,
-});
+export default defineConfig([
+  {
+    entry: ["eslint/index.ts"],
+    shims: true,
+    clean: true,
+    dts: true,
+    format: ["cjs", "esm"],
+    sourcemap: true,
+    outDir: "dist/eslint",
+  },
+  {
+    entry: ["prettier/index.ts"],
+    clean: true,
+    format: "esm",
+    dts: false,
+    outDir: "dist/prettier",
+  },
+]);
