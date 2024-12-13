115a0eaa2c6b37d2ebd40a09ade194db1ba71d79
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2024-12-13 23:11:38 +0100

    fix: run commitlint only on prs

--- src/cli/templates/commit-lint-ci.ts
+++ src/cli/templates/commit-lint-ci.ts
@@ -1,4 +1,5 @@
-export const commitLintCi = () =>
-  `      - name: Run commitlint check
+export const commitLintCi = () => `
+      - name: Check commit name
+        if: github.event_name == 'pull_request'
         run: npx commitlint -f \${{ github.event.pull_request.base.sha }}
 `;
