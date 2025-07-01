0ca04a7d21ce8765734eeee2c4541054c0d889c1
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2025-07-01 17:13:51 +0200

    fix: forbid axios import

--- src/eslint/plugins.ts
+++ src/eslint/plugins.ts
@@ -1,7 +1,6 @@
 //@ts-expect-error not working
 export { default as pluginComments } from "@eslint-community/eslint-plugin-eslint-comments";
 export { default as pluginAntfu } from "eslint-plugin-antfu";
-//@ts-expect-error not working too
 export { default as pluginImport } from "eslint-plugin-import";
 export { default as pluginNode } from "eslint-plugin-n";
 export { default as pluginUnicorn } from "eslint-plugin-unicorn";
