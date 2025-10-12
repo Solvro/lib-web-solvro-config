a9ea88cc7e5bfd56f4815c97286b3d1b84bff9d1
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2025-10-12 19:26:29 +0200

    chore: upgrade deps

--- knip.config.ts
+++ knip.config.ts
@@ -1,11 +1,13 @@
 import type { KnipConfig } from "knip";
 
 const config = {
-  ignore: [],
+  ignore: ["tests/nest-app/**", ".commitlintrc.js"],
   ignoreDependencies: [
     "prettier-plugin-packagejson",
     "prettier-plugin-tailwindcss",
     "@trivago/prettier-plugin-sort-imports",
+    "@commitlint/config-conventional",
+    "@vitest/coverage-v8",
   ],
 } satisfies KnipConfig;
 
