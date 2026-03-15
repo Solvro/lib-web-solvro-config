943df90c116f05359b217b85a776cbc4dc1ae9a8
Konrad Guzek <52281528+kguzek@users.noreply.github.com>
2026-03-15 21:44:17 +0100

    fix(prettier): plugin dependency resolution (#751)

--- knip.config.ts
+++ knip.config.ts
@@ -3,9 +3,6 @@ import type { KnipConfig } from "knip";
 const config = {
   ignore: ["tests/nest-app/**", ".commitlintrc.js"],
   ignoreDependencies: [
-    "prettier-plugin-packagejson",
-    "prettier-plugin-tailwindcss",
-    "@trivago/prettier-plugin-sort-imports",
     "@commitlint/config-conventional",
     "@vitest/coverage-v8",
   ],
