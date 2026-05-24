0f2bc1cfd8782944051ba00ccaae3e8f372cf878
Konrad Guzek <konrad@guzek.uk>
2026-05-24 21:11:51 +0200

    fix(cli): add url fragment

--- src/utils/print-outro.ts
+++ src/utils/print-outro.ts
@@ -4,12 +4,15 @@ import c from "picocolors";
 import { REPO_URL } from "../constants";
 import { formatLink } from "./format-log";
 
+const PROJECT_DOCS_URL =
+  "https://docs.solvro.pl/projects/creating#konfiguracja-repozytorium";
+
 export const printOutro = () => {
   p.log.success("Konfiguracja zakończona pomyślnie!");
   p.note(
     `\
 1. ${c.white(`${c.bold("Obowiązkowo")} zapoznaj się z ${c.cyan("dokumentacją tworzenia projektów")}:`)}
-   ${formatLink("https://docs.solvro.pl/projects/creating")}
+   ${formatLink(PROJECT_DOCS_URL)}
 2. ${c.white(`Pamiętaj o zostawianiu nam ${c.yellow("gwiazdek na GitHubie")}! ⭐`)}
    ${formatLink(REPO_URL)}`,
     "Kolejne kroki",
