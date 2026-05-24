a2abdb4649b4fbdc6f0351ce9edcc19ab9686272
Konrad Guzek <52281528+kguzek@users.noreply.github.com>
2026-05-24 16:37:21 +0200

    ci(preview): add -D and pnpm to comment url (#962)
    * ci(preview): add -D and pnpm to comment url
    
    * docs(readme): add preview badge
    
    * fix(knip): add pkg-pr-new exemption
    
    * fix(preview): stupid npm
    
    * fix(readme): badge URL

--- knip.config.ts
+++ knip.config.ts
@@ -4,9 +4,10 @@ const config = {
   ignore: ["tests/nest-app/**", ".commitlintrc.js"],
   ignoreDependencies: [
     "@commitlint/config-conventional",
-    "@vitest/coverage-v8",
     // this is actually used in src/eslint/stub.d.ts - possible bug in knip?
     "@typescript-eslint/utils",
+    // this is actually used in .github/workflows/preview.yml - maybe knip doesn't see `npm exec` as usage?
+    "pkg-pr-new",
   ],
 } satisfies KnipConfig;
 
