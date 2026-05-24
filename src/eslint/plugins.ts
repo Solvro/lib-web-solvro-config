dba9dd148bfc0aecda96432b9f9390baeffcdb3b
Konrad Guzek <konrad@guzek.uk>
2026-05-24 19:18:24 +0200

    feat(eslint): add eslint-plugin-regexp

--- src/eslint/plugins.ts
+++ src/eslint/plugins.ts
@@ -2,5 +2,6 @@ export { default as pluginComments } from "@eslint-community/eslint-plugin-eslin
 export { default as pluginAntfu } from "eslint-plugin-antfu";
 export { default as pluginImport } from "eslint-plugin-import";
 export { default as pluginNode } from "eslint-plugin-n";
+export { default as pluginRegexp } from "eslint-plugin-regexp";
 export { default as pluginUnicorn } from "eslint-plugin-unicorn";
 export { default as pluginUnusedImports } from "eslint-plugin-unused-imports";
